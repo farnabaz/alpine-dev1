50f45954f6430508ef13ced60252262612f97875
Farnabaz <farnabaz@gmail.com>
2023-06-29 18:13:15 +0330

    Update tokens.config.ts

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,12 +1,4 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
-  alpine: {
-    body: {
-      color: {
-          initial: 'orange',
-          dark: 'blue'
-      }
-    }
-  }
 })
