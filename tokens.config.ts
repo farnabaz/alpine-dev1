2a18dcd2eaa5e5fb847476c59c6314c4cfcdbf6b
Farnabaz <farnabaz@gmail.com>
2023-06-29 18:21:20 +0330

    qwe

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,17 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  alpine: {
+    body: {
+      backgroundColor: {
+        initial: '#FFFFFF1',
+        dark: '#0c0c0d'
+      },
+
+      color: {
+        initial: 'green',
+        dark: '#e4e4e7'
+      }
+    }
+  }
 })
